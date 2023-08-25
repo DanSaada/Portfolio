2b82e718b5be7c82a3aa6c0af690442acf375798
Dan Saada <dansaada2@gmail.com>
2023-08-26 01:22:57 +0300

    fixing the about page

--- components/Socials.js
+++ components/Socials.js
@@ -7,16 +7,16 @@ import {RiLinkedinLine, RiGithubLine, RiFacebookLine, RiInstagramLine} from 'rea
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <Link href={'https://www.linkedin.com/in/dan-saada-45a055250/'} className="hover:text-accent transition-all duration-300">
         <RiLinkedinLine />
       </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <Link href={'https://github.com/DanSaada'} className="hover:text-accent transition-all duration-300">
         <RiGithubLine />
       </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <Link href={'https://www.facebook.com/dan.saada.7'} className="hover:text-accent transition-all duration-300">
         <RiFacebookLine />
       </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <Link href={'https://www.instagram.com/dansa97/'} className="hover:text-accent transition-all duration-300">
         <RiInstagramLine />
       </Link>
     </div>
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,21 +1,41 @@
 import React, {useState} from 'react'
 
 // icons
-import {
-  FaHtml5,
-  FaCss3,
+import { 
+  FaJava,
+  FaPython,
+  // FaCplusplus,
+  // FaCSharp,
   FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
-} from 'react-icons'
+  FaHtml5, 
+  FaCss3, 
+  FaReact, 
+  FaFigma 
+} from 'react-icons/fa';
 
-import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
-} from 'react-icons/si'
+import { 
+  SiNextdotjs, 
+  SiFramer, 
+  SiAdobexd, 
+  SiAdobephotoshop 
+} from 'react-icons/si';
+
+import { IoCodeSlash } from 'react-icons/io5';
+
+
+
+// import {
+//   DiSqllite,
+//   DiMongodb,
+//   DiArduino,
+//   DiAndroid,
+//   DiLinux
+// } from 'react-icons/di';
+
+// import {
+//   RiAssemblyLine,
+// } from 'react-icons/ri';
+
 
 // data
 const aboutData = [
@@ -23,20 +43,24 @@ const aboutData = [
     title: 'skills',
     info: [
       {
-        title: 'Web Development',
+        title: 'Programming Languages',
         icons: [
+          <FaJava />,
+          <IoCodeSlash />,
+          // <FaCplusplus />,
+          // <FaCSharp />,
+          <FaPython />,
+          // <DiSqllite />,
+          // <RiAssemblyLine />,
           <FaHtml5 />,
           <FaCss3 />,
           <FaJs />,
           <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
         ],
       },
       {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        title: '',
+        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />, <SiNextdotjs />, <SiFramer />],
       },
     ],
   },
@@ -44,12 +68,12 @@ const aboutData = [
     title: 'awards',
     info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        title: 'Optimization Competition',
+        stage: '2022',
       },
       {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
+        title: 'Badge of honour',
+        stage: '2020',
       },
     ],
   },
@@ -97,6 +121,9 @@ import Circles from '../../components/Circles'
 import {motion} from 'framer-motion'
 import {fadeIn} from '../../variants'
 
+// counter
+import CountUp from 'react-countup'
+
 
 const About = () => {
   const [index, setIndex] = useState(0)
@@ -112,8 +139,63 @@ const About = () => {
         <Avatar />
       </motion.div>
       <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-        <div className='flex-1 flex flex-col justify-center'>text</div>
-        <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+        {/* text */}
+        <div className='flex-1 flex flex-col justify-center'>
+          <motion.h2 variants={fadeIn('right', 0.2)} 
+                     initial="hidden"
+                     animate="show"
+                     exit="hidden"
+                     className='h2'>
+            Captivating <span className='text-accent'>stories</span> birth magnificent designs.
+          </motion.h2>
+          <motion.p variants={fadeIn('right', 0.4)} 
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    className='max-2-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+          Results-driven software developer with a passion for problem solving and 
+          innovation. Proficient in Java and JavaScript, with hands-on experience 
+          building full web applications. Strong collaborator, leveraging excellent 
+          communication skills to work effectively in cross-functional teams. 
+          Eager to contribute expertise and enthusiasm to a software development role.
+          </motion.p>
+           {/* counters */}
+           <motion.div variants={fadeIn('right', 0.2)} 
+                       initial="hidden"
+                       animate="show"
+                       exit="hidden"
+                       className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8'>
+            <div className='flex flex-1 xl:gap-x-6'>
+               {/* experience */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp srart={0} end={3} duration={5} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Years of experience</div>
+              </div>
+              {/* projects */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp srart={0} end={35} duration={5} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Finished projects</div>
+              </div>
+              {/* winning awards */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp srart={0} end={4} duration={5} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>winning awards</div>
+              </div>
+            </div>
+           </motion.div>
+        </div>
+        {/* info */}
+        <motion.div variants={fadeIn('left', 0.4)} 
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
           {aboutData.map((item, itemIndex) => {
               return (
@@ -130,17 +212,28 @@ const About = () => {
               );
             })}
           </div>
-          <div>
+          <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
             {aboutData[index].info.map((item, itemIndex) => {
               return (
-                <div key={itemIndex}>
+                <div key={itemIndex}
+                     className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'>
                   {/* title */}
-                  <div>{item.title}</div>
+                  <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                  <div className='hidden md:flex'>-</div>
+                  <div>{item.stage}</div>
+                  <div className='flex gap-x-4'>
+                     {/* icons */}
+                     {item.icons?.map((icon, itemIndex) => {
+                      return <div className='text-2xl text-white'>{icon}</div>;
+                     })}
+                  </div>
+                 
+
                 </div>
               );
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
