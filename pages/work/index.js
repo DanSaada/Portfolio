20842170fc5b277a70e30e3d04b0bff3ed5f0f4f
Dan Saada <dansaada2@gmail.com>
2023-09-03 12:37:41 +0300

    adding content to the "projects" section

--- pages/work/index.js
+++ pages/work/index.js
@@ -30,7 +30,7 @@ const Work = () => {
               animate= 'show'
               exit='hidden'
               className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
-             something something somthing
+             Take a stroll through a collection of my projects that reflect my journey as a developer.
             </motion.p>
           </div>
 
