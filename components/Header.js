fd5c4ef6eea2e89c1d456f48f33010aa65413917
Dan Saada <dansaada2@gmail.com>
2023-09-04 11:01:06 +0300

    fixed the format of the logo

--- components/Header.js
+++ components/Header.js
@@ -10,10 +10,10 @@ import Socials from "../components/Socials"
 const Header = () => {
   return <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
     <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+      <div className="flex flex-row justify-between items-center gap-y-6 py-8">
         {/* logo */}
         <Link href={'/'}>
-          <Image src={'/dan-saada-logo7.png'}
+          <Image src={'/dan-saada-logo10.png'}
                  width={220} 
                  height={48} 
                  alt="" 
